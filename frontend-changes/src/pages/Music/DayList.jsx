9ec64e0304c6be1332377a233c1b51d050caa76a
gtanvi58 <tanvi.cva@gmail.com>
2024-07-06 17:17:23 +0530

    added daylist buttons

--- frontend-changes/src/pages/Music/DayList.jsx
+++ frontend-changes/src/pages/Music/DayList.jsx
@@ -1,33 +1,10 @@
-// import React from 'react'
-// import ReactPlayer from 'react-player'
-// import styles from './RecommendedArtists.scss';
-// import classNames from 'classnames/bind';
-
-// const cx = classNames.bind(styles);
-// // Render a YouTube video player
-
-// function DayList(){
-//     return(
-//         <div className={cx('daylist')}>
-//             <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' light={true} width={'200px'} height={'300px'}/>
-//         </div>
-//     )
-//     // return(
-//     //     <div className={cx('react-player')}>
-//     //     <title>hellooo</title>
-//     //      <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' light={true} width={'200px'} height={'300px'}/>
-//     // </div>
-//     // )
-   
-// }
-
-// export default DayList;
-
-
 import React from 'react';
 import ReactPlayer from 'react-player';
 import styles from './DayList.scss';
 import classNames from 'classnames/bind';
+import { FcMusic } from "react-icons/fc";
+import config from '~/config';
+import { NavLink } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
@@ -36,6 +13,20 @@ const videoUrls = [
     'https://www.youtube.com/watch?v=ysz5S6PUM-U',
     'https://www.youtube.com/watch?v=ScMzIvxBSi4'
 ];
+
+const handlePlayGame = (audio_id) => {
+    // Implement logic to play a game related to the video URL
+    console.log(`Playing a game related to: ${audio_id}`);
+    // Navigate to the specified route using react-router-dom NavLink
+    // Example of navigating to magicTiles route using config
+    // Replace with your actual logic based on your routes
+    // <NavLink
+    //     to={config.routes.magicTiles(audio_id)}
+    //     key={audio_id}
+    //     target="_blank"
+    //     rel="noopener noreferrer"
+    // ></NavLink>
+};
 
 function DayList() {
     return (
@@ -51,9 +42,22 @@ function DayList() {
                                 url={url}
                                 light={true}
                                 width={'400px'}
-                                height={'600px'}
+                                height={'550px'}
                                 className={cx('player')}
                             />
+                            <NavLink
+                                to={config.routes.magicTiles('bad-guy')}
+                                key={'bad-guy'}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <button className={cx('play-game-button')} onClick={() => handlePlayGame('bad-guy')}>
+                                    <FcMusic className={cx('button-icon')} />
+                                    <span className={cx('button-text')}>
+                                        Play Magic Tiles
+                                    </span>
+                                </button>
+                            </NavLink>
                         </div>
                     ))}
                 </div>
@@ -63,5 +67,3 @@ function DayList() {
 }
 
 export default DayList;
-
-
