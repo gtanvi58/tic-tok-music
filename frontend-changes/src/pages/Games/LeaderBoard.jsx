eb1b24f7d3961dfd5d3193e61bec7cebb9a2cd04
Siddarth-Suresh <65844402+Siddarth-Suresh@users.noreply.github.com>
2024-07-07 14:44:22 -0400

    Fixed Games Leaderboard Styling

--- frontend-changes/src/pages/Games/LeaderBoard.jsx
+++ frontend-changes/src/pages/Games/LeaderBoard.jsx
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { NavLink } from "react-router-dom";
 import classNames from 'classnames/bind';
 import styles from './LeaderBoard.scss';
-import config from '~/config';
+import config from '../../config';
 import axios from 'axios';
 import { useState } from 'react';
+
 
 const cx = classNames.bind(styles);
 
@@ -51,15 +52,17 @@ function LeaderBoard(props) {
         <div className={cx('leaderboard')}>
             <div className={cx('chart-container')}>
                 <div className={cx('header-container')}>
-                <div className={cx('song-header')}>SONG</div>
-                    <div className={cx('chart-header')}>LEADERBOARD</div>
-                    <div className={cx('score-header')}>SCORE</div>
+                <div className={cx('song-header')}>Pos</div>
+                <div className={cx('player-header')}>Player</div>
+                    <div className={cx('score-header')}>Score</div>
                 </div>
                 {props.leaderBoardItems.map((data, index) => (
                     <div className={cx('chart-row')}>
                     <div className={cx('chart-position')}>{index+1}</div>
                     <div className={cx('chart-details')}>
+                    <div className={cx('chart-position')}>
                         <p className={cx('chart-artist')}>{data.Username}</p>
+                        </div>
                     </div>
                     <div className={cx('chart-stats')}>
                         <div className={cx('chart-stat')}>{data.Score}</div>
